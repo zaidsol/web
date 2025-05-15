66f2b21d6381719e0440190383a8dff70efccde9
zaidsol <zaiducp387@gmail.com>
2025-05-15 13:49:04 +0500

    Update header.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <div className="mt-20 p-3">
-      <img
+      <image
         src="./start.PNG"
         alt="Header Background"
         className="w-full object-cover"
