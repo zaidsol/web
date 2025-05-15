1b429a91e9ec7f0bf4427ad2b06fbd8f1fc6bdbf
zaidsol <zaiducp387@gmail.com>
2025-05-15 14:29:50 +0500

    Update page.tsx

--- app/arrival/page.tsx
+++ app/arrival/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Navbar from '../components/navbar';
 import Header from '../components/header';
-import Products from '../products/page';
+
 import Footer from '../components/footer';
 
 export default function Arrival() {
