b79fc2a69814fe0cf62db63d14f8ad5c59ea2792
zaidsol <zaiducp387@gmail.com>
2025-05-15 14:00:04 +0500

    Update page.tsx

--- app/arrival/page.tsx
+++ app/arrival/page.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Navbar from '../components/navbar'
-import Header from '../components/header'
-import Products from '../products/page'
-import Footer from '../components/footer'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from '../components/navbar';
+import Header from '../components/header';
+import Products from '../products/page';
+import Footer from '../components/footer';
 
 export default function Arrival() {
   return (
     <div>
-      <Navbar></Navbar>
-      <Header></Header>
-      <Products></Products>
-      <Footer></Footer>
+      <Head>
+        <title>New Arrivals - Shop the Latest Products</title>
+        <meta name="description" content="Explore the newest arrivals in our shop. Find the best deals on top products!" />
+      </Head>
+      <Navbar />
+      <Header />
+      <Products />
+      <Footer />
     </div>
-  )
+  );
 }
+
