170aa8d753e75a0d53292d89e815207a0db8b194
zaidsol <zaiducp387@gmail.com>
2025-05-15 14:09:19 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import Navbar from './components/navbar'
 import Slider from './slider/page'
-import Header from './components/header'
-import Products from './products/page'
-import Footer from './components/footer'
+
 
 
 export default function Home() {
